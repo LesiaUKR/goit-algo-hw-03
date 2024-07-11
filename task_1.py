800be98db2226b4a2f5c359338fe3a57158a7ec9
Lesia Soloviova <solovyova.lesya@gmail.com>
2024-07-11 11:51:14 +0300

    fix: add error handlings for task_1

--- task_1.py
+++ task_1.py
@@ -5,33 +5,36 @@ import argparse
 # ANSI escape codes for colored output
 COLOR_BLUE = "\033[94m"
 COLOR_RESET = "\033[0m"
+COLOR_RED = "\033[91m"
 
 def copy_and_sort_files(source_dir, dest_dir="dist"):
       # Конвертуємо шлях директорії у об'єкт Path
       source_dir = Path(source_dir)
       dest_dir = Path(dest_dir)
    
-      # Перевіряємо, чи існує вихідна директорія
-      if not source_dir.exists():
-         print(f"{COLOR_BLUE}Source directory does not exist!{COLOR_RESET}")
+      if not source_dir.exists():   # Перевіряємо, чи існує вихідна директорія
+         print(f"{COLOR_BLUE}Вихідна директорія не існує!{COLOR_RESET}")
          return # Якщо директорія не існує, виводимо повідомлення і завершуємо виконання функції
-     # Перевіряємо, чи існує директорія призначення
-      if not dest_dir.exists():
-        #  директорія призначення не існує, створюємо її
-         dest_dir.mkdir()
-      # Ітеруємося по всіх елементах у вихідній директорії
-      for item in source_dir.iterdir():
-         if item.is_dir():
-               # Рекурсивно обробляємо піддиректорії
-               copy_and_sort_files(item, dest_dir)
-         elif item.is_file():
+     
+      if not dest_dir.exists(): # Перевіряємо, чи існує директорія призначення
+          try:
+             dest_dir.mkdir() #  директорія призначення не існує, створюємо її
+          except Exception as e:
+            print(f"{COLOR_RED}Не вдалося створити директорію призначення: {e}{COLOR_RESET}")
+            return
+          
+      for item in source_dir.iterdir():# Ітеруємося по всіх елементах у вихідній директорії
+          try:  
+              if item.is_dir(): # Перевіряємо, чи є елемент директорією
+               copy_and_sort_files(item, dest_dir) # Рекурсивно обробляємо піддиректорії
+              elif item.is_file():
                # Отримуємо розширення файлу
                extension = item.suffix[1:] # Видаляємо крапку з розширення
-                # Створюємо піддиректорію для файлів з таким розширенням
-               dest_subdir = dest_dir / extension
-               dest_subdir.mkdir(exist_ok=True)
-               # Копіюємо файл до відповідної піддиректорії
-               shutil.copy2(item, dest_subdir)
+               dest_subdir = dest_dir / extension  # Це шлях до піддиректорії, яку потрібно створити з іменем директорії таким як розширення у файлів
+               dest_subdir.mkdir(exist_ok=True) # Створюємо піддиректорію, якщо вона не існує
+               shutil.copy2(item, dest_subdir) # Копіюємо файл до відповідної піддиректорії
+          except Exception as e:
+            print(f"{COLOR_RED}Не вдалося обробити {item}: {e}{COLOR_RESET}")
 
 def main():
     parser = argparse.ArgumentParser(description='Копіювання файлів з сортуванням за розширенням.')
@@ -40,14 +43,19 @@ def main():
     # Додаємо аргумент для директорії призначення
     parser.add_argument('dest', type=Path, nargs='?', default='dist', help='Шлях до директорії призначення (за замовчуванням "dist")')
     
-    # Парсимо аргументи командного рядка
-    args = parser.parse_args()
+
+    args = parser.parse_args() # Парсимо аргументи командного рядка
     
-    # Перевіряємо, чи існує вихідна директорія і чи є вона директорією
-    if not args.src.is_dir():
+    if not args.src.is_dir(): # Перевіряємо, чи існує вихідна директорія і чи є вона директорією
         print(f"Помилка: Директорія {args.src} не існує або не є директорією.")
         return
-    args.dest.mkdir(parents=True, exist_ok=True)
+    
+     # Створюємо директорію призначення, якщо вона не існує
+    try:
+        args.dest.mkdir(parents=True, exist_ok=True)
+    except Exception as e:
+        print(f"{COLOR_RED}Не вдалося створити директорію призначення: {e}{COLOR_RESET}")
+        return
     copy_and_sort_files(args.src, args.dest)
     print(f"Файли успішно скопійовані до {args.dest}")
 
